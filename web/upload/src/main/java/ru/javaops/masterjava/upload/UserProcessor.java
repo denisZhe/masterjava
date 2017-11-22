417b3e898a8d975c8b82184abd182e303aab30a1
denisZhe <d.zherevchuk@gmail.com>
2017-11-22 22:15:31 +0300

    Prepare to HW5

--- web/upload/src/main/java/ru/javaops/masterjava/upload/UserProcessor.java
+++ web/upload/src/main/java/ru/javaops/masterjava/upload/UserProcessor.java
@@ -1,28 +1,99 @@
 package ru.javaops.masterjava.upload;
 
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
+import ru.javaops.masterjava.persist.DBIProvider;
+import ru.javaops.masterjava.persist.dao.UserDao;
 import ru.javaops.masterjava.persist.model.User;
 import ru.javaops.masterjava.persist.model.UserFlag;
+import ru.javaops.masterjava.xml.schema.ObjectFactory;
+import ru.javaops.masterjava.xml.util.JaxbParser;
 import ru.javaops.masterjava.xml.util.StaxStreamProcessor;
 
+import javax.xml.bind.JAXBException;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.events.XMLEvent;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
+@Slf4j
 public class UserProcessor {
+    private static final int NUMBER_THREADS = 4;
 
-    public List<User> process(final InputStream is) throws XMLStreamException {
-        final StaxStreamProcessor processor = new StaxStreamProcessor(is);
-        List<User> users = new ArrayList<>();
+    private static final JaxbParser jaxbParser = new JaxbParser(ObjectFactory.class);
+    private static UserDao userDao = DBIProvider.getDao(UserDao.class);
+
+    private ExecutorService executorService = Executors.newFixedThreadPool(NUMBER_THREADS);
+
+    @AllArgsConstructor
+    public static class FailedEmails {
+        public String emailsOrRange;
+        public String reason;
+
+        @Override
+        public String toString() {
+            return emailsOrRange + " : " + reason;
+        }
+    }
+
+    /*
+     * return failed users chunks
+     */
+    public List<FailedEmails> process(final InputStream is, int chunkSize) throws XMLStreamException, JAXBException {
+        log.info("Start processing with chunkSize=" + chunkSize);
+
+        Map<String, Future<List<String>>> chunkFutures = new LinkedHashMap<>();  // ordered map (emailRange -> chunk future)
+
+        int id = userDao.getSeqAndSkip(chunkSize);
+        List<User> chunk = new ArrayList<>(chunkSize);
+        val processor = new StaxStreamProcessor(is);
+        val unmarshaller = jaxbParser.createUnmarshaller();
 
         while (processor.doUntil(XMLEvent.START_ELEMENT, "User")) {
-            final String email = processor.getAttribute("email");
-            final UserFlag flag = UserFlag.valueOf(processor.getAttribute("flag"));
-            final String fullName = processor.getReader().getElementText();
-            final User user = new User(fullName, email, flag);
-            users.add(user);
+            ru.javaops.masterjava.xml.schema.User xmlUser = unmarshaller.unmarshal(processor.getReader(), ru.javaops.masterjava.xml.schema.User.class);
+            final User user = new User(id++, xmlUser.getValue(), xmlUser.getEmail(), UserFlag.valueOf(xmlUser.getFlag().value()));
+            chunk.add(user);
+            if (chunk.size() == chunkSize) {
+                addChunkFutures(chunkFutures, chunk);
+                chunk = new ArrayList<>(chunkSize);
+                id = userDao.getSeqAndSkip(chunkSize);
+            }
         }
-        return users;
+
+        if (!chunk.isEmpty()) {
+            addChunkFutures(chunkFutures, chunk);
+        }
+
+        List<FailedEmails> failed = new ArrayList<>();
+        List<String> allAlreadyPresents = new ArrayList<>();
+        chunkFutures.forEach((emailRange, future) -> {
+            try {
+                List<String> alreadyPresentsInChunk = future.get();
+                log.info("{} successfully executed with already presents: {}", emailRange, alreadyPresentsInChunk);
+                allAlreadyPresents.addAll(alreadyPresentsInChunk);
+            } catch (InterruptedException | ExecutionException e) {
+                log.error(emailRange + " failed", e);
+                failed.add(new FailedEmails(emailRange, e.toString()));
+            }
+        });
+        if (!allAlreadyPresents.isEmpty()) {
+            failed.add(new FailedEmails(allAlreadyPresents.toString(), "already presents"));
+        }
+        return failed;
+    }
+
+    private void addChunkFutures(Map<String, Future<List<String>>> chunkFutures, List<User> chunk) {
+        String emailRange = String.format("[%s-%s]", chunk.get(0).getEmail(), chunk.get(chunk.size() - 1).getEmail());
+        Future<List<String>> future = executorService.submit(() -> userDao.insertAndGetConflictEmails(chunk));
+        chunkFutures.put(emailRange, future);
+        log.info("Submit chunk: " + emailRange);
     }
 }
